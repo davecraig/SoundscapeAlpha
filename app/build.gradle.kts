3d48e2d45f1632f5c8412527beec66129d79aa39
davec <davecraig@unbalancedaudio.com>
2024-06-29 21:38:21 +0100

    Change min  SDK from 28 to 30
    The minimum SDK of 28 didn't include the TextToSpeech API being used,
    so this change ups it to 30. Unfortunately that rules out any of my old
    phones and this moves from a 2018 release to 2020. I think this is as new
    as we should go, and later we can investigate reducing it back down.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.scottishtecharmy.soundscape"
-        minSdk = 28
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
