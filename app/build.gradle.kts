7b3a07dd6c2384eec070060d0180c0344af4617e
davecraig <davecraig@unbalancedaudio.com>
2024-07-02 14:44:53 +0000

    Bump version to 0.0.8, version code 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.0.7"
+        versionCode = 8
+        versionName = "0.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
