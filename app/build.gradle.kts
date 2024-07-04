2f657eefd4761348503be0f1fe297c4b1c4e4f24
davecraig <davecraig@unbalancedaudio.com>
2024-07-04 09:02:31 +0000

    Bump version to 0.0.10, version code 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.scottishtecharmy.soundscape"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10
-        versionName = "0.0.9 ${versionName})"
+        versionCode = 11
+        versionName = "0.0.10"
 
         buildConfigField("String", "VERSION_NAME", "\"${versionName}\"")
 
